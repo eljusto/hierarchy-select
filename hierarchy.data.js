9eabdc065851838df85d6e90b860a06d4ec25ffe
Boris Zhidkov <boris.zhidkov@gmail.com>
2014-06-16 22:56:51 +0400

    initial commit

---
+++ hierarchy.data.js
@@ -0,0 +1,43 @@
+var hierarchy = [
+  { id: 100,
+    name: 'Pearl Strands Finished',
+    subitems: [
+      { id: 10,
+        name: 'Akoya',
+        subitems: [
+          { id: 1, name: 'Necklaces' },
+          { id: 2, name: 'Bracelets' }
+        ]
+    },
+    { id: 19,
+      name: 'Freshwater',
+      subitems: [ 
+        { id: 1, name: 'Necklaces' },
+        { id: 2, name: 'Bracelets' }
+      ]
+    }
+    ]
+},
+{
+  id: 102,
+  name: 'Pearl Jewellery',
+  subitems: [
+    { id: 23,
+      name: 'Necklaces/Pendants',
+      classes: [
+        { id: 4, name: 'FW' },
+        { id: 5, name: 'Tahitlan' },
+      ]
+  },
+  {
+    id: 15,
+    name: 'Earrings',
+    subitems: [
+      { id: 4, name: 'FW' },
+      { id: 5, name: 'Tahitlan' }
+    ]
+  }
+  ]
+}
+
+];
