bf017597bb50f8a4928767b4165b96da2cda3577
Boris Zhidkov <boris.zhidkov@gmail.com>
2014-06-24 15:21:23 +0400

    rewritten in jQuery plugin style. alpha-version

--- hierarchy-select.js
+++ hierarchy-select.js
@@ -1,76 +1,93 @@
-H = {
+(function ($) {
 
-  baseElement: null,
-  data: [],
-  selectsList: [],
-  onChange: function(){},
-  create: function(el, options) {
-   var list = options.hierarchy;
-   var values = options.values;
-   if (options.onChange) {
-     this.onChange = options.onChange;
-   }
-    this.data = this.flattenList([], list, 0);
-    this.baseElement = el;
-    if (!values || !values.length) {
-      values = [0];
-    }
-    var parentId = 0;
-    for (var i = 0, l = values.length; i < l; i ++) {
-      var select = this.createSelect(this.getChildren(parentId));
-      parentId = values[i];
-      select.val(values[i]);
-      this.selectsList.push(select);
-      el.append(select);
-    }
-    return this;
-  }, 
+var _h = {
 
-  getChildren: function(parentId) {
-    return this.data.filter(function(element){
-      return element.parentId  == parentId;
-    });
-  },
-  flattenList: function(total, list, parentId) {
-    var _this = this;
-    if (list.length > 0) {
-      list.forEach(function(item){
-        total.push({'id': item.id, 'title': item.name, 'parentId': parentId});
-        if (item.subitems){
-          total =  _this.flattenList(total, item.subitems, item.id);
-        }
-      }); 
-    }
-    return total;
-  },
+    data: [],
+    selectsList: [],
+    levels: [],
+    onChange: function(){},
 
-  createSelect: function(list) {
-    var _this = this;
-    var select = $('<select />');
+    getChildren: function(parentId, category) {
+      return this.data.filter(function(element){
+        return element.parentId == parentId;
+      });
+    },
+
+    flattenList: function(total, list, parentId) {
+      var _this = this;
+      if (list.length > 0) {
+        list.forEach(function(item){
+          total.push({'id': item.id, 'title': item.name, 'parentId': parentId});
+          if (item.subitems){
+            total =  _this.flattenList(total, item.subitems, item.id);
+          }
+        }); 
+      }
+      return total;
+    },
+
+    createSelect: function(el, list, levelNum) {
+      var _this = this;
+      var select = $('<select />');
+      var levelId;
+      if (levelNum !== undefined) {
+        levelId = _this.levels[levelNum].id;
+        select.attr('name', levelId);
+      }
       select.append( $('<option></option>').val(-1).html('-') );
-    $.each(list, function(idx, item) {
-      select.append( $('<option></option>').val(item.id).html(item.title) );
-    });
-    select.change(function(){
-      var idx = _this.selectsList.indexOf(select);
-      for (var i = _this.selectsList.length-1; i > idx; i-- ) {
-        _this.selectsList[i].remove();
-        _this.selectsList.splice(i, 1);
-      }
-      var parentId = select.val();
-      var childrenItems = _this.getChildren(parentId);
-      if (childrenItems.length) {
-        var childSelect = _this.createSelect(childrenItems);
-        _this.selectsList.push(childSelect);
-        _this.baseElement.append(childSelect);
-      }
-      var selectValues = [];
-      for (var i = 0, l = _this.selectsList.length; i < l; i ++) {
-        var v = $(_this.selectsList[i]).val();
-        selectValues.push(v);
-      }
-      _this.onChange(selectValues);
-    });
-    return select;
-  },
+      $.each(list, function(idx, item) {
+        select.append( $('<option></option>').val(item.id).html(item.title) );
+      });
+      select.change(function(){
+        var idx = _h.selectsList.indexOf(select);
+        for (var i = _this.selectsList.length-1; i > idx; i-- ) {
+          _this.selectsList[i].remove();
+          _this.selectsList.splice(i, 1);
+        }
+        var parentId = select.val();
+        var childrenItems = _this.getChildren(parentId, levelId);
+        if (childrenItems.length) {
+          var childSelect = _this.createSelect(el, childrenItems, idx + 1);
+          _this.selectsList.push(childSelect);
+          el.append(childSelect);
+        }
+        var selectValues = [];
+        for (var i = 0, l = _this.selectsList.length; i < l; i ++) {
+          var v = $(_this.selectsList[i]).val();
+          selectValues.push({ 
+            categoryId: _this.levels[i].id, 
+            categoryName:_this.levels[i].name, 
+            value: v 
+          });
+        }
+        _this.onChange(selectValues);
+      });
+      return select;
+    },
 };
+
+  $.fn.hierarchySelect = function(options) {
+    var settings = $.extend({
+    'onChange' : function(){}, 
+    'values': [0],
+    }, options);
+
+      var hierarchy = settings.hierarchy[0];
+      var list = hierarchy.items;
+      var values = settings.values;
+      
+      _h.data = _h.flattenList([], list, 0);
+      _h.onChange = settings.onChange;
+      _h.levels = hierarchy.levels;
+
+      var parentId = 0;
+      for (var i = 0, l = values.length; i < l; i ++) {
+        var select = _h.createSelect(this, _h.getChildren(parentId), i);
+        parentId = values[i];
+        select.val(values[i]);
+        _h.selectsList.push(select);
+        this.append(select);
+      }
+      return this;
+    };
+})(jQuery);
--- hierarchy.data.js
+++ hierarchy.data.js
@@ -1,43 +1,51 @@
-var hierarchy = [
-  { id: 100,
-    name: 'Pearl Strands Finished',
-    subitems: [
-      { id: 10,
-        name: 'Akoya',
-        subitems: [
+var hierarchy = [{
+  levels: [
+    {id: 'dept', name: 'Dept'},
+    {id: 'subdept', name: 'SubDept'},
+    {id: 'classname', name: 'ClassName'},
+  ],
+  items: 
+    [
+    { id: 100,
+      name: 'Pearl Strands Finished',
+      subitems: [
+        { id: 10,
+          name: 'Akoya',
+          subitems: [
+            { id: 1, name: 'Necklaces' },
+            { id: 2, name: 'Bracelets' }
+          ]
+      },
+      { id: 19,
+        name: 'Freshwater',
+        subitems: [ 
           { id: 1, name: 'Necklaces' },
           { id: 2, name: 'Bracelets' }
         ]
-    },
-    { id: 19,
-      name: 'Freshwater',
-      subitems: [ 
-        { id: 1, name: 'Necklaces' },
-        { id: 2, name: 'Bracelets' }
-      ]
-    }
-    ]
-},
-{
-  id: 102,
-  name: 'Pearl Jewellery',
-  subitems: [
-    { id: 23,
-      name: 'Necklaces/Pendants',
-      subitems: [
-        { id: 4, name: 'FW' },
-        { id: 5, name: 'Tahitlan' },
+      }
       ]
   },
   {
-    id: 15,
-    name: 'Earrings',
+    id: 102,
+    name: 'Pearl Jewellery',
     subitems: [
-      { id: 4, name: 'FW' },
-      { id: 5, name: 'Tahitlan' }
+      { id: 23,
+        name: 'Necklaces/Pendants',
+        subitems: [
+          { id: 4, name: 'FW' },
+          { id: 5, name: 'Tahitlan' },
+        ]
+    },
+    {
+      id: 15,
+      name: 'Earrings',
+      subitems: [
+        { id: 4, name: 'FW' },
+        { id: 5, name: 'Tahitlan' }
+      ]
+    }
     ]
   }
   ]
 }
-
 ];
