e34207433c0d31daab6cfcfe51534df7c79b00df
Boris Zhidkov <boris.zhidkov@gmail.com>
2014-07-21 10:42:16 +0400

    fixed an issue with multiple instances of hiearchy-select

--- hierarchy-select.js
+++ hierarchy-select.js
@@ -1,87 +1,92 @@
 (function ($) {
 
-var _h = {
-
-    data: [],
-    selectsList: [],
-    selectBlocksList: [],
-    levels: [],
-    onChange: function(){},
-
-    getChildren: function(parentId, category) {
-      return this.data.filter(function(element){
-        return element.parentId == parentId;
-      });
-    },
-
-    flattenList: function(total, list, parentId) {
-      var _this = this;
-      if (list.length > 0) {
-        list.forEach(function(item){
-          total.push({'id': item.id, 'title': item.name, 'parentId': parentId});
-          if (item.subitems){
-            total =  _this.flattenList(total, item.subitems, item.id);
-          }
-        }); 
-      }
-      return total;
-    },
-
-    createSelect: function(el, list, levelNum) {
-      var _this = this;
-      var select = $('<select />');
-      var levelId;
-      if (levelNum !== undefined) {
-        levelId = _this.levels[levelNum].id;
-        select.attr('name', levelId);
-      }
-      select.append( $('<option></option>').val(-1).html('-') );
-      $.each(list, function(idx, item) {
-        select.append( $('<option></option>').val(item.id).html(item.title) );
-      });
-      select.change(function(){
-        var idx = _h.selectsList.indexOf(select);
-        for (var i = _this.selectBlocksList.length-1; i > idx; i-- ) {
-          _this.selectsList[i].remove();
-          _this.selectsList.splice(i, 1);
-          _this.selectBlocksList[i].remove();
-          _this.selectBlocksList.splice(i, 1);
-        }
-        var parentId = select.val();
-        var childrenItems = _this.getChildren(parentId, levelId);
-        if (childrenItems.length) {
-          var childSelectBlock = _this.createSelect(el, childrenItems, idx + 1);
-          el.append(childSelectBlock);
-        }
-        var selectValues = [];
-        for (var i = 0, l = _this.selectsList.length; i < l; i ++) {
-          var v = $(_this.selectsList[i]).val();
-          var s = (_this.selectsList[i])[0];
-          var valueName = s.options[s.selectedIndex].innerHTML;
-          selectValues.push({ 
-            categoryId: _this.levels[i].id, 
-            categoryName:_this.levels[i].name, 
-            value: v,
-            valueName: valueName
-          });
-        }
-        _this.onChange(selectValues);
-      });
-      var selectBlock = $(this.selectWrapper);
-      selectBlock.addClass(this.wrapperClass);
-      selectBlock.append(select);
-      this.selectsList.push(select);
-      this.selectBlocksList.push(selectBlock);
-      return selectBlock;
-    },
-};
-
   $.fn.hierarchySelect = function(options) {
+
+    var _h = {
+
+      data: [],
+      selectsList: [],
+      selectBlocksList: [],
+      levels: [],
+      onChange: function(){},
+
+      getChildren: function(parentId, category) {
+        return this.data.filter(function(element){
+          return element.parentId == parentId;
+        });
+      },
+
+      flattenList: function(total, list, parentId) {
+        var _this = this;
+        if (list.length > 0) {
+          list.forEach(function(item){
+            total.push({'id': item.id, 'title': item.name, 'parentId': parentId});
+            if (item.subitems){
+              total =  _this.flattenList(total, item.subitems, item.id);
+            }
+          }); 
+        }
+        return total;
+      },
+
+      createSelect: function(el, list, levelNum) {
+        var _this = this;
+        var select = $('<select class=""/>');
+        var levelId;
+        if (levelNum !== undefined) {
+          levelId = _this.levels[levelNum].id;
+          select.attr('name', levelId);
+          select.addClass('form-control');
+        }
+        select.append( $('<option></option>').val(-1).html('-') );
+        $.each(list, function(idx, item) {
+          select.append( $('<option></option>').val(item.id).html(item.title) );
+        });
+        select.change(function(){
+          var idx = _h.selectsList.indexOf(select);
+          for (var i = _this.selectBlocksList.length-1; i > idx; i-- ) {
+            _this.selectsList[i].remove();
+            _this.selectsList.splice(i, 1);
+            _this.selectBlocksList[i].remove();
+            _this.selectBlocksList.splice(i, 1);
+          }
+          var parentId = select.val();
+          var childrenItems = _this.getChildren(parentId, levelId);
+          if (childrenItems.length) {
+            var childSelectBlock = _this.createSelect(el, childrenItems, idx + 1);
+            el.append(childSelectBlock);
+          }
+          var selectValues = [];
+          for (var i = 0, l = _this.selectsList.length; i < l; i ++) {
+            var v = $(_this.selectsList[i]).val();
+            if (v === '-1') break;
+            var s = (_this.selectsList[i])[0];
+            var valueName = s.options[s.selectedIndex].innerHTML;
+            selectValues.push({ 
+              categoryId: _this.levels[i].id, 
+              categoryName:_this.levels[i].name, 
+              value: v,
+              valueName: valueName
+            });
+          }
+          _this.onChange(selectValues);
+        });
+        var selectBlock = $(this.selectWrapper);
+        selectBlock.addClass(this.wrapperClass);
+        selectBlock.append(select);
+        this.selectsList.push(select);
+        this.selectBlocksList.push(selectBlock);
+        return selectBlock;
+      }
+    };
+
+    _h.selectsList = [];
+    _h.selectBlocksList = [];
     var settings = $.extend({
-    'onChange' : function(){}, 
-    'values': [0],
-    'selectWrapper': '<div>',
-    'wrapperClass' : 'hs-wrapper'
+      'onChange' : function(){},
+      'values': [0],
+      'selectWrapper': '<li>',
+      'wrapperClass' : 'hs-wrapper'
     }, options);
 
       var hierarchy = settings.hierarchy[0];
